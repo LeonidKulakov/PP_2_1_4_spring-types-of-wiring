dd59dcbe53dbedb069b6594194843a03481d3972
leoni <kylakov_leonid@mail.ru>
2024-04-30 16:31:15 +0300

    30.04.24

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,12 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    @Override
+    public String toString() {
+        return "Вывод: Не сиди на игле Х_Х";
+    }
 }
